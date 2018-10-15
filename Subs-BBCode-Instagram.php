46c320718e8c58b8ad29eec751a3d7fabd144a08
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-15 10:24:55 -0500

    v1.13 - November 15th, 2015
    v1.13 - November 15th, 2015
    o Modified validation pattern to recognize URLs without the "/p" part as valid.
    o Modified auto-embed regex pattern to require the "http" or "https" part of the URL.

--- Subs-BBCode-Instagram.php
+++ Subs-BBCode-Instagram.php
@@ -72,7 +72,7 @@ function BBCode_Instagram_Validate(&$tag, &$data, &$disabled)
 	{
 		if (strpos($data, 'http://') !== 0 && strpos($data, 'https://') !== 0)
 			$data = 'http://' . $data;
-		$pattern = '#(http|https)://(|(.+?).)instagram.com/p/((.+?){10})(|(\?|/)(.+?))#i';
+		$pattern = '#(http|https)://(|(.+?).)instagram.com/(?:p/)?((.+?){10})(|(\?|/)(.+?))#i';
 		if (!preg_match($pattern, $data, $parts))
 			return ($tag['content'] = $txt['instagram_no_post_id']);
 		$data = $parts[4];
@@ -84,6 +84,7 @@ function BBCode_Instagram_Validate(&$tag, &$data, &$disabled)
 		$height = $modSettings['instagram_default_height'];
 	$tag['content'] = '<div style="' . (empty($width) ? '' : 'max-width: ' . $width . 'px;') . (empty($height) ? '' : 'max-height: ' . $height . 'px;') . '"><div class="instagram-wrapper">' .
 		'<iframe src="https://instagram.com/p/' . $data .'/embed/" scrolling="no" frameborder="' . $frameborder . '"></iframe></div></div>';
+	//echo $tag['content']; exit;
 }
 
 function BBCode_Instagram_LoadTheme()
@@ -102,7 +103,7 @@ function BBCode_Instagram_Settings(&$config_vars)
 
 function BBCode_Instagram_Embed(&$message)
 {
-	$pattern = '~(?<=[\s>\.(;\'"]|^)(?:https?\:\/\/)?(?:www\.)?instagram.com\/(?:p/)?[A-Za-z0-9_\-]+\??[/\w\-_\~%@\?;=#}\\\\]?~';
+	$pattern = '~(?<=[\s>\.(;\'"]|^)(https?\:\/\/)(?:www\.)?instagram.com\/(?:p/)?[A-Za-z0-9_\-]+\??[/\w\-_\~%@\?;=#}\\\\]?~';
 	$message = preg_replace($pattern, '[instagram]$0[/instagram]', $message);
 }
 
